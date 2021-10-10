e632284c84987d2beab2688b39192a88ab342e51
jcalbrecht <jcalbrecht85@gmail.com>
2021-10-09 23:09:16 -0500

    rename folder

--- packages/brownie/scripts/deploy_master.py
+++ packages/brownie/scripts/deploy_master.py
@@ -1,5 +1,4 @@
 from brownie import accounts, config, network, Master, Contract
-import math
 # Set key in brownie config
 acct = accounts.add(config['wallets']['from_key'])
 print(network.show_active())
@@ -7,5 +6,4 @@ print(network.show_active())
 
 def main():
     deployed = Master.deploy({'from': acct})
-    # deployed = Contract("0x2bFF4244783eceE98cD8D24712E860c19ef0A661")
     return deployed, acct
--- packages/brownie/scripts/deploy_requester.py
+++ packages/brownie/scripts/deploy_requester.py
@@ -6,14 +6,10 @@ print(network.show_active())
 
 def main():
     deployed = Consumer.deploy({'from': account})
-    print('Deployed By: ', account.address, '\nAt: ', deployed.address)
     link_token_address = "0xb0897686c545045aFc77CF20eC7A532E3120E0F1"
     amount = 0.001 * 10**18 
     transfer_token(link_token_address, deployed.address, amount)
 
-
-    # deployed.withdrawLink({'from' : account})
-    # deployed.requestBytes("Mafia", {'from': account})
     return deployed, account
 
 def transfer_token(token_address, to_address, amount):
